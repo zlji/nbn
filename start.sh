643c4056584875d9b4e48ee5aeb2025a8ca8e20b
ruiban <ruiban@077n.com>
2019-07-02 15:46:24 -0400

    update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+net=$(docker network ls | grep docker)
+if [ -z "$net" ]; then
+  echo "Create network docker:"
+  docker network create docker
+fi
+
 $(dirname $0)/.docker/compose.sh up -d
